dd89aa309486c70b75918709ce6ff5e945ffb2c8
Linuk Perera <163678566+LinukPerera@users.noreply.github.com>
2025-07-13 00:47:55 +0530

    Add files via upload

---
+++ Lab1G1.vhd
@@ -0,0 +1,54 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    11:39:12 03/16/2022 
+-- Design Name: 
+-- Module Name:    Lab1G1 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Lab1G1 is
+PORT(
+	A:IN Bit;
+	Aout:OUT Bit;
+
+end Lab1G1;
+
+architecture Behavioral of Lab1G1 is
+
+begin
+
+
+
+end Behavioral;
+
+architecture AND1 of Lab1G1 is
+
+begin
+
+
+
+end AND1;
+
---
+++ Labex2.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    11:52:45 03/16/2022 
+-- Design Name: 
+-- Module Name:    Labex2 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Labex2 is
+    Port ( A : in  STD_LOGIC;
+           B : out  STD_LOGIC);
+end Labex2;
+
+architecture Behavioral of Labex2 is
+
+begin
+
+
+end Behavioral;
+
